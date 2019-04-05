26ce6b6db685d98488675335dd041413bba0112c
K SRAVAN KUMAR <sravan.kumar1691@gmail.com>
2019-04-05 14:35:20 +0530

    Path finding

---
+++ Nav Mesh Agent/Assets/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public NavMeshAgent meshAgent;
+    public Camera cam;
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray= cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                meshAgent.SetDestination(hit.point);
+            } 
+        }    
+    }
+}
